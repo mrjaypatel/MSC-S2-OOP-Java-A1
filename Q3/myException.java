813a538071021ec86105c9983c32123557538fd2
Jay Patel <p1998jay@gmail.com>
2021-09-07 16:04:10 +0530

    Output SS added

--- Q3/myException.java
+++ Q3/myException.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class myExxception{
+class myException{
 	 public static void main(String args[]){
         System.out.println("##### Q1 Started ####");
         Scanner sc= new Scanner(System.in);
